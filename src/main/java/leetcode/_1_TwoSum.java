3d529a40a19045e8123cd4819eb950ad28d76277
maverickmanish <bhardwajoperations@gmail.com>
2023-12-30 08:17:33 -0800

    formatting and indentation

--- src/main/java/leetcode/_1_TwoSum.java
+++ src/main/java/leetcode/_1_TwoSum.java
@@ -11,23 +11,22 @@ import java.util.Map;
 
 public class _1_TwoSum {
 
-	public static void main(String[] args) {
-		System.out.println(Arrays.toString(twoSum(new int[] { 2, 7, 11, 15 }, 9)));
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
 
-	}
+    }
 
-	public static int[] twoSum(int[] nums, int target)
-	{
-		int i=0;
-		Map<Integer,Integer> sumChecker=new HashMap<>();
-		while(i<nums.length)
-		{	int x=target-nums[i];
-			if(sumChecker.containsKey(x))
-			return new int[]{sumChecker.get(x),i};
-			else
-			sumChecker.put(nums[i], i);
-			i++;
-		}
-		return null;
-	}
+    public static int[] twoSum(int[] nums, int target) {
+        int i = 0;
+        Map<Integer, Integer> sumChecker = new HashMap<>();
+        while (i < nums.length) {
+            int x = target - nums[i];
+            if (sumChecker.containsKey(x))
+                return new int[]{sumChecker.get(x), i};
+            else
+                sumChecker.put(nums[i], i);
+            i++;
+        }
+        return null;
+    }
 }
